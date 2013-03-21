7abc5ec6ac1d9d002ecaa772470ee4d1b0a7dff7
Chris Ballinger <chrisballinger@gmail.com>
2013-03-20 17:23:05 -0700

    provision db and users

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,6 @@
-default['ow_sentry']['secret_databag_name'] = "secrets"
-default['ow_sentry']['secret_databag_item_name'] = "sentry"
+default['ow_sentry']['secret_databag_name'] = "sentry"
+default['ow_sentry']['secret_databag_item_name'] = "users"
+default['ow_sentry']['db_host'] = "127.0.0.1"
+default['ow_sentry']['db_user'] = "postgres"
+default['ow_sentry']['db_port'] = 5432
+default['ow_sentry']['db_name'] = 'sentry'
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,8 +7,41 @@
 # Licensed under AGPLv3
 #
 
-#secrets = Chef::EncryptedDataBagItem.load(node['ow_sentry']['secret_databag_name'] , node['ow_sentry']['secret_databag_item_name'])
+users_bag = Chef::EncryptedDataBagItem.load(node['ow_sentry']['secret_databag_name'] , node['ow_sentry']['secret_databag_item_name'])
+users = users_bag["users"]
 
+db_user = node['ow_sentry']['db_user']
+db_port = node['ow_sentry']['db_port']
+db_host = node['ow_sentry']['db_host']
+db_pass = node['postgresql']['password']['postgres']
+db_name = node['ow_sentry']['db_name']
+
+
+# Setup postgresql database
+postgresql_database db_name do
+  connection ({
+        :host => db_host, 
+        :port => db_port, 
+        :username => db_user, 
+        :password => db_pass
+  })
+  action :create
+end
+
+node.set['sentry']['superusers'] = users
+
+# Database settings
+node.set["sentry"]["settings"]["databases"] = {
+  "default" => {
+    "ENGINE" => "django.db.backends.postgresql_psycopg2",
+    "NAME" => "#{db_name}",
+    "USER" => "#{db_user}",
+    "PASSWORD" => "#{db_pass}",
+    "HOST" => "#{db_host}",
+    "PORT" => "#{db_port}"
+  }}
+
+# node.save
 
 include_recipe "sentry::instance"
 
